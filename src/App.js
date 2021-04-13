8866d1170868d6f25d933b7189e7ce565d9dee2f
ThomasMLawrence <tmlawrence54@gmail.com>
2021-04-13 15:35:32 -0700

    Basic stat sheet added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import {  makeStyles } from '@material-ui/core/styles';
 
-function App() {
+const useStyles = makeStyles ({
+  containerStyle: {
+    backgroundColor: '#d7cec7',
+    height: '400px',
+    marginTop: '40px',
+  },
+  paperStyle: {
+    height: '100px',
+    textAlign: 'center'
+  },
+  attrName: {
+    fontSize: '20px',
+    fontWeight: '600'
+  },
+  field: {
+    padding: '10px'
+  }
+})
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <Container
+      maxWidth="sm"
+      className={classes.containerStyle}
+    >
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>
+            <Grid container spacing={1}>
+              <Grid className={classes.attrName} item xs={12}>Dexterity</Grid>
+              <Grid className={classes.field} item xs={4}>value</Grid>
+              <Grid className={classes.field} item xs={4}>modifier</Grid>
+              <Grid className={classes.field} item xs={4}>save</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>
+          <Grid container spacing={1}>
+              <Grid className={classes.attrName} item xs={12}>Intelligence</Grid>
+              <Grid className={classes.field} item xs={4}>value</Grid>
+              <Grid className={classes.field} item xs={4}>modifier</Grid>
+              <Grid className={classes.field} item xs={4}>save</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>
+          <Grid container spacing={1}>
+              <Grid className={classes.attrName} item xs={12}>Strength</Grid>
+              <Grid className={classes.field} item xs={4}>value</Grid>
+              <Grid className={classes.field} item xs={4}>modifier</Grid>
+              <Grid className={classes.field} item xs={4}>save</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>
+          <Grid container spacing={1}>
+              <Grid className={classes.attrName} item xs={12}>Wisdom</Grid>
+              <Grid className={classes.field} item xs={4}>value</Grid>
+              <Grid className={classes.field} item xs={4}>modifier</Grid>
+              <Grid className={classes.field} item xs={4}>save</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>
+          <Grid container spacing={1}>
+              <Grid className={classes.attrName} item xs={12}>Constitution</Grid>
+              <Grid className={classes.field} item xs={4}>value</Grid>
+              <Grid className={classes.field} item xs={4}>modifier</Grid>
+              <Grid className={classes.field} item xs={4}>save</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paperStyle}>
+          <Grid container spacing={1}>
+              <Grid className={classes.attrName} item xs={12}>Charisma</Grid>
+              <Grid className={classes.field} item xs={4}>value</Grid>
+              <Grid className={classes.field} item xs={4}>modifier</Grid>
+              <Grid className={classes.field} item xs={4}>save</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+              <Grid className={classes.field} item xs={4}>0</Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  </div>
+);
 }
 
-export default App;
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#d7cec7',
+            dark: '#565656'
+        },
+        secondary: {
+            main: '#c09f80',
+            dark: '#76323f'
+        }
+    }
+})
+
+export default theme;
