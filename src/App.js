d0abf8f9a77e9c9141927cb0f85fa0e36f25dfbb
andrew <aruskampwhite@gmail.com>
2021-04-20 16:22:29 -0700

    Added notes section.

--- src/App.js
+++ src/App.js
@@ -1,134 +1,124 @@
-import React, { useState } from 'react'
-import { Grid } from '@material-ui/core'
-import { AppBar } from '@material-ui/core'
-import { Typography } from '@material-ui/core'
-import { Toolbar } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import { Menu } from '@material-ui/core'
-import { MenuItem } from '@material-ui/core'
-import { Button, IconButton } from '@material-ui/core'
-import { ArrowBack, LibraryBooks } from '@material-ui/icons'
-import { Fade } from '@material-ui/core'
-import Character from './Character'
-import Login from './Login'
-import Details from './Details'
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import { AppBar } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Menu } from "@material-ui/core";
+import { MenuItem } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
+import { ArrowBack, LibraryBooks } from "@material-ui/icons";
+import { Fade } from "@material-ui/core";
+import Character from "./Character";
+import Login from "./Login";
+import Details from "./Details";
+import Notes from "./Notes";
 
-
-
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   page: {
-    position: 'absolute',
+    position: "absolute",
   },
   container: {
-    display: 'grid',
-    girdGap: theme.spacing(3)
+    display: "grid",
+    girdGap: theme.spacing(3),
   },
   gridItem: {
-    height: '100%'
+    height: "100%",
   },
   column: {
-    height: '100vh',
-    margin: '10px'
+    height: "100vh",
+    margin: "10px",
   },
   titleBar: {
-    backgroundColor: '#76323f',
-    height: '60px',
-    paddingBottom: '5px'
+    backgroundColor: "#76323f",
+    height: "60px",
+    paddingBottom: "5px",
   },
   tool: {
-    display: 'flex',
-    justifyContent: 'space-between'
+    display: "flex",
+    justifyContent: "space-between",
   },
   toolLeft: {
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center",
   },
   back: {
     alighItems: "center",
     justify: "center",
-    backgroundColor: '#565656',
-    minHeight: '100vh'
+    backgroundColor: "#565656",
+    minHeight: "100vh",
   },
   card: {
-    backgroundColor: '#d7cec7',
-    padding: '10px',
-    marginTop: '2em'
+    backgroundColor: "#d7cec7",
+    padding: "10px",
+    marginTop: "2em",
   },
   classMenu: {
-    marginLeft: '16px'
+    marginLeft: "16px",
   },
   loginButton: {
-    marginRight: '16px'
-  }
-
-}))
-
-
+    marginRight: "16px",
+  },
+}));
 
 export default function App() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [details, setDetails] = useState(false)
-  const [login, setLogin] = useState(true)
-  
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [details, setDetails] = useState(false);
+  const [login, setLogin] = useState(true);
+
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
+    setAnchorEl(event.currentTarget);
+  };
 
   const handleMenuClose = () => {
-      setAnchorEl(null)
-  }
+    setAnchorEl(null);
+  };
 
-  const swapDetails = () =>{
-    setDetails((prev) => !prev)
-  }
+  const swapDetails = () => {
+    setDetails((prev) => !prev);
+  };
 
-  const fadeLogin = () =>{
-    setLogin((prev) => !prev)
-  }
- 
-  
+  const fadeLogin = () => {
+    setLogin((prev) => !prev);
+  };
 
   return (
-  <div className={classes.back}>
-    <AppBar className={classes.titleBar} position="static">
-      <Toolbar className={classes.tool}>
-        <div className={classes.toolLeft}>
-          <IconButton
-            variant="contained"
-            onClick={fadeLogin}
-            className={classes.loginButton}
-          >
-            <ArrowBack/>
-          </IconButton>
-          <Typography variant="h4" className={classes.title}>
-            Sheets United
-          </Typography>
-        </div>
-        <div class={classes.toolRight}>
-          <IconButton
-            variant="contained"
-            onClick={swapDetails}
-          >
-            <LibraryBooks/>
-          </IconButton>
-          <Button
+    <div className={classes.back}>
+      <AppBar className={classes.titleBar} position="static">
+        <Toolbar className={classes.tool}>
+          <div className={classes.toolLeft}>
+            <IconButton
+              variant="contained"
+              onClick={fadeLogin}
+              className={classes.loginButton}
+            >
+              <ArrowBack />
+            </IconButton>
+            <Typography variant="h4" className={classes.title}>
+              Sheets United
+            </Typography>
+          </div>
+          <div class={classes.toolRight}>
+            <IconButton variant="contained" onClick={swapDetails}>
+              <LibraryBooks />
+            </IconButton>
+            <Button
               className={classes.classMenu}
               variant="contained"
               onClick={handleClick}
               edge="end"
-          >
+            >
               Choose a Class
-          </Button>
-          <Menu 
+            </Button>
+            <Menu
               anchorEl={anchorEl}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
               keepMounted
-              transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+              transformOrigin={{ vertical: "top", horizontal: "center" }}
               open={Boolean(anchorEl)}
               onClose={handleMenuClose}
-          >
+            >
               <MenuItem onClick={handleMenuClose}>Fighter</MenuItem>
               <MenuItem onClick={handleMenuClose}>Barbarian</MenuItem>
               <MenuItem onClick={handleMenuClose}>Paladin</MenuItem>
@@ -142,46 +132,43 @@ export default function App() {
               <MenuItem onClick={handleMenuClose}>Warlock</MenuItem>
               <MenuItem onClick={handleMenuClose}>Druid</MenuItem>
               <MenuItem onClick={handleMenuClose}>Artificer</MenuItem>
-          </Menu>
-
-        </div>
-      </Toolbar>
-    </AppBar>
-    <Fade in={!details & !login}>
-      <Grid
-            className={classes.page}
-            container
-            justify="center"
-            alightItems="center"
-            spacing={0}
-        > 
-        <Character></Character>
-      </Grid>
-    </Fade>
-    <Fade in={details & !login}>
-      <Grid
-            className={classes.page}
-            container
-            justify="center"
-            alightItems="center"
-            spacing={0}
-        > 
-        <Details></Details>
-      </Grid>
-    </Fade>
-    <Fade in={login}>
-      <Grid
-        className={classes.page}
-        container
-        justify="center"
-        alightItems="center"
-        spacing={0}
-      >
-        <Login fadeLogin={fadeLogin}></Login>
-      </Grid>
-    </Fade>
-    
-  </div>
-);
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Fade in={!details & !login}>
+        <Grid
+          className={classes.page}
+          container
+          justify="center"
+          alightItems="center"
+          spacing={0}
+        >
+          <Character></Character>
+        </Grid>
+      </Fade>
+      <Fade in={details & !login}>
+        <Grid
+          className={classes.page}
+          container
+          justify="center"
+          alightItems="center"
+          spacing={0}
+        >
+          <Details></Details>
+        </Grid>
+      </Fade>
+      <Fade in={login}>
+        <Grid
+          className={classes.page}
+          container
+          justify="center"
+          alightItems="center"
+          spacing={0}
+        >
+          <Login fadeLogin={fadeLogin}></Login>
+        </Grid>
+      </Fade>
+    </div>
+  );
 }
-
--- src/Character.js
+++ src/Character.js
@@ -1,124 +1,125 @@
-import React, { useState } from 'react'
-import { Paper } from '@material-ui/core'
-import { Grid } from '@material-ui/core'
-import { Dialog, TextField } from '@material-ui/core'
-import { withStyles } from '@material-ui/core/styles'
-import Stat from './Stat'
-import CoreStats from './CoreStats'
-import Proficiency from './Proficiency'
-import InfoBar from './InfoBar'
-
-
-
-
+import React, { useState } from "react";
+import { Paper } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { Dialog, TextField } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import Stat from "./Stat";
+import CoreStats from "./CoreStats";
+import Proficiency from "./Proficiency";
+import InfoBar from "./InfoBar";
+import Notes from "./Notes";
 
 const styles = {
-    paperStyle: {
-        height: '100px',
-        textAlign: 'center',
-        '&:hover': {
-            opacity: '0.75'
-        },
-        
+  paperStyle: {
+    height: "100px",
+    textAlign: "center",
+    "&:hover": {
+      opacity: "0.75",
     },
-    dialogContainer: {
-        display: 'flex',
-        justifyContent: 'center',
-        textAlign: 'center',
-        paddingBottom: '10px'
-    },
-    field: {
-        padding: '20px'
-    },
-    page: {
-        position: 'absolute',
-    },
-    card: {
-        backgroundColor: '#d7cec7',
-        padding: '10px',
-        marginTop: '2em'
-    },
-    column: {
-        height: '100vh',
-        margin: '10px'
-    },
-    gridItem: {
-        height: '100%'
-    },
-}
+  },
+  dialogContainer: {
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    paddingBottom: "10px",
+  },
+  field: {
+    padding: "20px",
+  },
+  page: {
+    position: "absolute",
+  },
+  card: {
+    backgroundColor: "#d7cec7",
+    padding: "10px",
+    marginTop: "2em",
+  },
+  column: {
+    height: "100vh",
+    margin: "10px",
+  },
+  gridItem: {
+    height: "100%",
+  },
+};
 
+function Character(props) {
+  const { classes } = props;
+  const [open, setOpen] = useState(false);
 
+  const handleClose = () => {
+    setOpen(false);
+  };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-function Character(props){
-    const { classes } = props
-    const [open, setOpen] = useState(false)
-
-    const handleClose = () => {
-        setOpen(false)
-      }
-  
-      const handleClickOpen = () => {
-        setOpen(true)
-      }
-
-
-    return(
-            <Grid
-            className={classes.page}
-            container
-            justify="center"
-            alightItems="center"
-            spacing={0}
-        > 
-            <Grid className={classes.column} xs={3} item>
-
+  return (
+    <Grid
+      className={classes.page}
+      container
+      justify="center"
+      alightItems="center"
+      spacing={1}
+    >
+      <Grid className={classes.column} xs={3} item></Grid>
+      <Grid className={classes.column} xs={3} item>
+        <Grid container direction="column">
+          <Paper className={classes.card}>
+            <Grid className={classes.gridItem} xs={12} item>
+              <InfoBar></InfoBar>
             </Grid>
-            <Grid className={classes.column} xs={3} item>
-                <Grid container direction="column" >
-                <Paper className={classes.card} >
-                    <Grid className={classes.gridItem} xs={12} item>
-                        <InfoBar></InfoBar>
-                    </Grid>
-                </Paper>
-                <Paper className={classes.card} >
-                    <Grid className={classes.gridItem} xs={12} item>
-                        <CoreStats handleClickOpen={handleClickOpen}></CoreStats>
-                    </Grid>
-                </Paper>
-                <Paper className={classes.card} >
-                    <Grid className={classes.gridItem} xs={12} item>
-                        <Stat handleClickOpen={handleClickOpen}></Stat>
-                    </Grid>
-                </Paper>
-                <Paper className={classes.card} >
-                    <Grid className={classes.gridItem} xs={12} item>
-                        <Proficiency></Proficiency>
-                    </Grid>
-                </Paper>
-                </Grid>
+          </Paper>
+          <Paper className={classes.card}>
+            <Grid className={classes.gridItem} xs={12} item>
+              <CoreStats handleClickOpen={handleClickOpen}></CoreStats>
             </Grid>
-            
-            <Grid className={classes.column} xs={3} item>
-                
+          </Paper>
+          <Paper className={classes.card}>
+            <Grid className={classes.gridItem} xs={12} item>
+              <Stat handleClickOpen={handleClickOpen}></Stat>
             </Grid>
-            <Dialog open={open} onClose={handleClose}>        
-                <Grid container className={classes.dialogContainer} spacing={0}>
-                    <Grid className={classes.field} item xs={4}><TextField label="Health"></TextField></Grid>
-                    <Grid className={classes.field} item xs={4}><TextField label="AC"></TextField></Grid>
-                    <Grid className={classes.field} item xs={4}><TextField label="Hit Die"></TextField></Grid>
-                    <Grid className={classes.field} item xs={4}><TextField label="Initiative"></TextField></Grid>
-                    <Grid className={classes.field} item xs={4}><TextField label="Movement"></TextField></Grid>
-                    <Grid className={classes.field} item xs={4}><TextField label="Proficiency"></TextField></Grid>
-                </Grid>
-            </Dialog>
-            
+          </Paper>
+          <Paper className={classes.card}>
+            <Grid className={classes.gridItem} xs={12} item>
+              <Proficiency></Proficiency>
+            </Grid>
+          </Paper>
         </Grid>
-        
-    )
+      </Grid>
 
+      <Grid className={classes.column} xs={3} item>
+        <Paper className={classes.card}>
+          <Grid className={classes.gridItem} xs={12} item>
+            <Notes></Notes>
+          </Grid>
+        </Paper>
+      </Grid>
+      <Dialog open={open} onClose={handleClose}>
+        <Grid container className={classes.dialogContainer} spacing={0}>
+          <Grid className={classes.field} item xs={4}>
+            <TextField label="Health"></TextField>
+          </Grid>
+          <Grid className={classes.field} item xs={4}>
+            <TextField label="AC"></TextField>
+          </Grid>
+          <Grid className={classes.field} item xs={4}>
+            <TextField label="Hit Die"></TextField>
+          </Grid>
+          <Grid className={classes.field} item xs={4}>
+            <TextField label="Initiative"></TextField>
+          </Grid>
+          <Grid className={classes.field} item xs={4}>
+            <TextField label="Movement"></TextField>
+          </Grid>
+          <Grid className={classes.field} item xs={4}>
+            <TextField label="Proficiency"></TextField>
+          </Grid>
+        </Grid>
+      </Dialog>
+    </Grid>
+  );
 }
 
-
-
-export default withStyles(styles)(Character)
+export default withStyles(styles)(Character);
