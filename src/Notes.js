3305ccb6ef6385b4dd7f23e4146723714ae9c597
andrew <aruskampwhite@gmail.com>
2021-04-22 13:10:45 -0700

    Added tabs to Notes

--- src/Notes.js
+++ src/Notes.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Paper } from "@material-ui/core";
-import { sizing } from "@material-ui/system";
-import { Grid } from "@material-ui/core";
-import { Dialog, TextField, TextareaAutosize } from "@material-ui/core";
+import { Grid, Box } from "@material-ui/core";
+import PropTypes from "prop-types";
+import { Tabs, Tab, TextField } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
@@ -29,13 +29,42 @@ const styles = {
   },
 };
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={4}>{children}</Box>}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
 function Notes(props) {
   const { classes } = props;
   //   const [open, setOpen] = useState(false);
-
-  //   const handleClose = () => {
-  //     setOpen(false);
-  //   };
+  const [page, setPage] = useState(0);
+  const handleChange = (event, newValue) => {
+    setPage(newValue);
+  };
   //   const handleClickOpen = () => {
   //     setOpen(true);
   //   };
@@ -44,16 +73,24 @@ function Notes(props) {
     <Grid container spacing={0}>
       <Grid item xs={12}>
         <Paper className={classes.paperStyle}>
-          <TextField
-            className={classes.textAreaStyle}
-            id="Note-area"
-            label="Character Notes"
-            rowsMax={12}
-            placeholder="What kind of adventures have you had?"
-            multiline
-            fullWidth="true"
-            height="100%"
-          ></TextField>
+          <Tabs value={page} onChange={handleChange}>
+            <Tab label="Attacks" {...a11yProps(0)} />
+            <Tab label="Equipment" {...a11yProps(1)} />
+            <Tab label="Spells" {...a11yProps(2)} />
+            <Tab label="Notes" {...a11yProps(3)} />
+          </Tabs>
+          <TabPanel value={page} index={3}>
+            <TextField
+              className={classes.textAreaStyle}
+              id="Note-area"
+              label="Character Notes"
+              rowsMax={12}
+              placeholder="What kind of adventures have you had?"
+              multiline
+              fullWidth="true"
+              height="100%"
+            ></TextField>
+          </TabPanel>
         </Paper>
       </Grid>
       {/* Once this is working, I'd like to add a dialog box that pops up for more entry */}
