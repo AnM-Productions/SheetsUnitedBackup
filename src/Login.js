77dbb9c49b44093f5dadf0a8d604f542bec11aba
Michael Lawrence <tml4@pdx.edu>
2021-05-18 14:07:19 -0700

    Finished create account page

--- src/Login.js
+++ src/Login.js
@@ -1,51 +1,52 @@
-import React from 'react'
-import { Paper, Typography, withStyles, Button, TextField } from '@material-ui/core'
-
-
+import React from "react";
+import {
+  Paper,
+  Typography,
+  withStyles,
+  Button,
+  TextField,
+  Grid,
+  Divider
+} from "@material-ui/core";
 
 const styles = {
-    loginPage: {
-        marginTop: '6em',
-        padding: '15px',
-        display: 'flex',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
-        width: '400px'
-    },
-    loginSubmit: {
-        width: '40%',
-        backgroundColor: '#4285F4',
-        color: 'white',
-        margin: '5px'
-    },
-    loginGuest: {
-        width: '40%',
-        backgroundColor: '#808080',
-        color: 'white',
-        margin: '5px'
-    },
-    loginField: {
-        width: '80%',
-        marginBottom: '2em',
-    }
+  loginPage: {
+    marginTop: "6em",
+    padding: "10px",
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    width: "40%",
+  },
+  loginContainer: {
+      justifyContent: "space-around"
+  },
+  loginItem: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    padding: "2px"
+  },
+  loginSubmit: {
+    width: "90%",
+    backgroundColor: "#4285F4",
+    color: "white",
+    margin: "5px",
+  },
+  loginField: {
+    width: "80%",
+    marginBottom: "2em",
+  },
+};
 
-}
+function Login(props) {
+  const { classes } = props;
 
-
-
-function Login (props){
-    const { classes } = props;
-
-
-
-    return (
-        <Paper
-            className={classes.loginPage}
-
-        >
-            <Typography variant="h5">
-                Please enter your credentials
-            </Typography>
+  return (
+    <Paper className={classes.loginPage}>
+      <Grid container spacing={0} className={classes.loginContainer}>
+        <Grid item xs={5} className={classes.loginItem}>
+            <Typography variant="h5">Please enter your credentials</Typography>
             <TextField label="username" className={classes.loginField}></TextField>
             <TextField label="password" className={classes.loginField}></TextField>
             <Button
@@ -55,18 +56,24 @@ function Login (props){
             >
                 Enter
             </Button>
+        </Grid>
+        <Divider orientation = "vertical"></Divider>
+        <Grid item xs={5} className={classes.loginItem}>
+            <Typography variant="h5">Don't have an account yet?</Typography>
+            <TextField label="new username" className={classes.loginField}></TextField>
+            <TextField label="new password" className={classes.loginField}></TextField>
+            <TextField label="confirm password" className={classes.loginField}></TextField>
             <Button
                 variant="contained"
                 onClick={props.fadeLogin}
-                className={classes.loginGuest}
+                className={classes.loginSubmit}
             >
-                Guest
+                Create Account
             </Button>
-        </Paper>
-
-    );
-
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
 
-
-export default withStyles(styles)(Login)
+export default withStyles(styles)(Login);
